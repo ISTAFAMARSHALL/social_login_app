4ae20ae893d8e2d1e5f731dcd7a38da719c749ea
istafamarshall <istafa.a.marshall@gmail.com>
2023-10-05 21:44:07 -0400

    create images folder

--- backend/passport.js
+++ backend/passport.js
@@ -4,6 +4,9 @@ const passport = require("passport");
 const GOOGLE_CLIENT_ID = "403940930490-9p5upakgv7g8brgiignanr4frs6r3rsv.apps.googleusercontent.com"
 const GOOGLE_CLIENT_SECRET = "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"
 
+GITHUB_CLIENT_ID = "Iv1.6185e0c26f25211b";
+GITHUB_CLIENT_SECRET = "281eeb9cb30021c34b774c21f048e9c73810c990";
+
 passport.use(
     new GoogleStrategy(
   {
@@ -15,6 +18,19 @@ passport.use(
     done(null,profile );
   }
   )
+);
+
+passport.use(
+    new GithubStrategy(
+    {
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "/auth/github/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+    done(null, profile);
+    }
+    )
 );
 
 passport.serializeUser((user,done)=>{

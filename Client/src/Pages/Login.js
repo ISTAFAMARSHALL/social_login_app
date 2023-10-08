51a32ab45570cf85ec932488f53abadbcaf6af75
istafamarshall <istafa.a.marshall@gmail.com>
2023-10-08 02:33:37 -0400

    update handleFacebookLogin

--- Client/src/Pages/Login.js
+++ Client/src/Pages/Login.js
@@ -5,6 +5,41 @@ function Login() {
 
     const handleFacebookLogin = () => {
         console.log('Facebook Login')
+        // Initialize the Facebook SDK
+        window.fbAsyncInit = function () {
+            window.FB.init({
+                appId: 'YOUR_FACEBOOK_APP_ID',
+                cookie: true,
+                xfbml: true,
+                version: 'v10.0',
+            });
+
+            // Trigger the Facebook login dialog
+            window.FB.login(
+            function (response) {
+                if (response.authResponse) {
+                // User is authenticated, you can fetch user details using the Graph API
+                window.FB.api('/me', function (user) {
+                    console.log('Facebook user:', user);
+                    // Handle user data and authentication here
+                });
+                } else {
+                // User canceled or failed to authenticate
+                console.log('Facebook login canceled or failed');
+                }
+            },
+            { scope: 'email' } // Define the requested permissions here
+            );
+        };
+
+        // Load the Facebook SDK asynchronously
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
     }
 
     const handleGoogleLogin = () => {
@@ -21,29 +56,31 @@ function Login() {
 
     return (
         <div>
-            <h1>Choose Login</h1>
+            
+            <h1>Choose your login method below.</h1>
+            
             <div id='wrapper'>
 
             <div onClick={handleFacebookLogin} > 
-                <img src={Facebook} alt='' /> 
+                <img src={""} alt='' /> 
                 Facebook 
             </div>
-
+            <br></br>
             <div onClick={handleGoogleLogin} > 
-                <img img src={Google} alt='' /> 
+                <img img src={""} alt='' /> 
                 Google 
             </div>
-
+            <br></br>
             <div onClick={handleGitHubLogin} > 
-                <img img src={Github} alt='' /> 
+                <img img src={""} alt='' /> 
                 Github 
             </div>
-
+            <br></br>
             <div onClick={handleLinkedInLogin} > 
-                <img img src={LinkedIn} alt='' /> 
+                <img img src={""} alt='' /> 
                 LinkedIn 
             </div>
-
+            <br></br>
             </div>
         </div>
     );

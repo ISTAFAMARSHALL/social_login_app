5797d03b6fe4fac0989abf5351a00e494bffeed8
istafamarshall <istafa.a.marshall@gmail.com>
2023-10-08 02:19:40 -0400

    update login.js

--- Client/src/Pages/Login.js
+++ Client/src/Pages/Login.js
@@ -3,27 +3,45 @@ import Github from "../Images/github.png";
 
 function Login() {
 
-    const google = () => {
-        window.open("http://localhost:3001/auth/google", "_self");
+    const handleFacebookLogin = () => {
+        console.log('Facebook Login')
     }
-    
-    const github = () => {
-        window.open("http://localhost:3001/auth/github", "_self");
-    };
+
+    const handleGoogleLogin = () => {
+        console.log('Google Login')
+    }
+
+    const handleGitHubLogin = () => {
+        console.log('GitHub Login')
+    }
+
+    const handleLinkedInLogin = () => {
+        console.log('LinkedIn Login')
+    }
 
     return (
         <div>
             <h1>Choose Login</h1>
             <div id='wrapper'>
 
-            <div onClick={google} > 
-                <img src={Google} alt='' /> 
+            <div onClick={handleFacebookLogin} > 
+                <img src={Facebook} alt='' /> 
+                Facebook 
+            </div>
+
+            <div onClick={handleGoogleLogin} > 
+                <img img src={Google} alt='' /> 
                 Google 
             </div>
 
-            <div onClick={github} > 
+            <div onClick={handleGitHubLogin} > 
                 <img img src={Github} alt='' /> 
                 Github 
+            </div>
+
+            <div onClick={handleLinkedInLogin} > 
+                <img img src={LinkedIn} alt='' /> 
+                LinkedIn 
             </div>
 
             </div>

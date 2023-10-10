9deadd0f8e65ff5c687ac9cfb818a02f4598322c
istafamarshall <istafa.a.marshall@gmail.com>
2023-10-10 03:46:28 -0400

    update facebook sdk

--- Client/src/Pages/Login.js
+++ Client/src/Pages/Login.js
@@ -1,61 +1,112 @@
 import Google from "../Images/google.png";
 import Github from "../Images/github.png";
+import { useEffect, useState } from "react";
 
 function Login() {
 
-    const handleFacebookLogin = () => {
-        console.log('Facebook Login')
-        // Initialize the Facebook SDK
-        window.fbAsyncInit = function () {
-            window.FB.init({
-                appId: 'YOUR_FACEBOOK_APP_ID',
-                cookie: true,
-                xfbml: true,
-                version: 'v10.0',
-            });
+  const handleFacebookLogin = () => {
+      console.log('Facebook Login')
+      // Initialize the Facebook SDK
+      FB.login(function(response) {
+        if (response.authResponse) {
+          // User is logged in, you can access the authResponse.accessToken here
+          const accessToken = response.authResponse.accessToken;
+          console.log('Facebook access token:', accessToken);
+        } else {
+          // User canceled the login or didn't authorize the app
+          console.log('Facebook login canceled.');
+        }
+      }, { scope: 'public_profile,email' }); // Specify the required permissions
+    
+  };
 
-            // Trigger the Facebook login dialog
-            window.FB.login(
-            function (response) {
-                if (response.authResponse) {
-                // User is authenticated, you can fetch user details using the Graph API
-                window.FB.api('/me', function (user) {
-                    console.log('Facebook user:', user);
-                    // Handle user data and authentication here
-                });
-                } else {
-                // User canceled or failed to authenticate
-                console.log('Facebook login canceled or failed');
-                }
-            },
-            { scope: 'email' } // Define the requested permissions here
-            );
-        };
 
-        // Load the Facebook SDK asynchronously
-        (function (d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    }
+    useEffect(() => {
+        /* global google */
+        google.accounts.id.initialize({
+          client_id: "403940930490-9p5upakgv7g8brgiignanr4frs6r3rsv.apps.googleusercontent.com",
+          callback: handleResponse
+        })
+    
+        google.accounts.id.renderButton(
+          document.getElementById("signInDiv"),
+          {theme: "outline", size: "large"}
+        );
+    
+        // google.accounts.id.prompt();
+    
+      }, []);
 
-    const handleGoogleLogin = () => {
-        gapi.load('auth2', function () {
-          gapi.auth2.init({
-            client_id: '403940930490-9p5upakgv7g8brgiignanr4frs6r3rsv.apps.googleusercontent.com',
-          }).then(function (auth2) {
-            auth2.signIn().then(function (googleUser) {
-              // User is authenticated, you can access user details from googleUser
-              const profile = googleUser.getBasicProfile();
-              console.log('Google user:', profile);
-              // Handle user data and authentication here
-            });
-          });
-        });
-    };
+    //   const handleResponse = (response) => {
+    //     if (response.credential) {
+    //         // User is authenticated, you can access user details from response.credential
+    //         const idToken = response.credential;
+    //         console.log('Google user ID token:', idToken);
+    //         // Handle user data and authentication here
+    //     } else {
+    //         // User canceled or failed to authenticate
+    //         console.error('Google login canceled or failed');
+    //     }
+    // }
+
+    const handleGoogleLogin = async () => {
+        
+          google.accounts.id.initialize({
+            client_id: "403940930490-9p5upakgv7g8brgiignanr4frs6r3rsv.apps.googleusercontent.com",
+            callback: handleResponse
+          })
+    
+        //   const handleResponse = (response) => {
+        //     if (response.credential) {
+        //         // User is authenticated, you can access user details from response.credential
+        //         const idToken = response.credential;
+        //         console.log('Google user ID token:', idToken);
+        //         // Handle user data and authentication here
+        //     } else {
+        //         // User canceled or failed to authenticate
+        //         console.error('Google login canceled or failed');
+        //     }
+        
+        // }
+      };
+
+      const handleResponse = (response) => {
+        if (response.credential) {
+            // User is authenticated, you can access user details from response.credential
+            const idToken = response.credential;
+            console.log('Google user ID token:', idToken);
+            // Handle user data and authentication here
+        } else {
+            // User canceled or failed to authenticate
+            console.error('Google login canceled or failed');
+        }
+
+    // const handleGoogleLogin = () => {
+    //     // Define the configuration for Google Identity Services
+    //     const gsiConfig = {
+    //       client_id: '403940930490-9p5upakgv7g8brgiignanr4frs6r3rsv.apps.googleusercontent.com',
+    //     };
+    
+    //     // Initialize Google Identity Services
+    //     const gsi = google.accounts.id.initialize(gsiConfig);
+    
+    //     // Prompt the user to select a Google account for authentication
+    //     gsi.prompt()
+    //       .then((response) => {
+    //         if (response.credential) {
+    //           // User is authenticated, you can access user details from response.credential
+    //           const idToken = response.credential;
+    //           console.log('Google user ID token:', idToken);
+    //           // Handle user data and authentication here
+    //         } else {
+    //           // User canceled or failed to authenticate
+    //           console.error('Google login canceled or failed');
+    //         }
+    //       })
+    //       .catch((error) => {
+    //         console.error('Error during Google login:', error);
+    //       });
+      };
 
     const handleGitHubLogin = () => {
         // Redirect the user to GitHub OAuth page
@@ -73,7 +124,11 @@ function Login() {
           });
         });
     };
-
+    
+    const t = FB.login(function(response){
+      // handle the response 
+    });
+    
     return (
         <div>
             
@@ -82,11 +137,15 @@ function Login() {
             <div id='wrapper'>
 
             <div onClick={handleFacebookLogin} > 
+            {/* <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+                </fb:login-button> */}
                 <img src={""} alt='' /> 
+                {t}
                 Facebook 
             </div>
             <br></br>
-            <div onClick={handleGoogleLogin} > 
+            <div onClick={handleGoogleLogin} >
+            <div id="signInDiv" ></div>
                 <img img src={""} alt='' /> 
                 Google 
             </div>

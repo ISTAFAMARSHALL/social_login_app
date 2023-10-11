006a2141cbb527e264e5d2f878e06b55e4ff9794
istafamarshall <istafa.a.marshall@gmail.com>
2023-10-11 13:35:03 -0400

    remove facebook code and update login.js code

---
+++ Client/src/Components/Google.js
@@ -0,0 +1,22 @@
+
+
+function Google() {
+    return (
+        <div>
+            <div id="g_id_onload"
+                data-client_id="4039403940930490-9p5upakgv7g8brgiignanr4frs6r3rsv.apps.googleusercontent.com40930"
+                data-login_uri="https://your.domain/your_login_endpoint">
+            </div>
+            <div class="g_id_signin"
+                data-type="standard"
+                data-size="large"
+                data-theme="outline"
+                data-text="sign_in_with"
+                data-shape="rectangular"
+                data-logo_alignment="left">
+            </div>
+        </div>
+    )
+}
+
+export default Google
--- Client/src/Pages/Login.js
+++ Client/src/Pages/Login.js
@@ -1,160 +1,84 @@
-import Google from "../Images/google.png";
-import Github from "../Images/github.png";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 
 function Login() {
 
-  const handleFacebookLogin = () => {
-      console.log('Facebook Login')
-      // Initialize the Facebook SDK
-      FB.login(function(response) {
-        if (response.authResponse) {
-          // User is logged in, you can access the authResponse.accessToken here
-          const accessToken = response.authResponse.accessToken;
-          console.log('Facebook access token:', accessToken);
-        } else {
-          // User canceled the login or didn't authorize the app
-          console.log('Facebook login canceled.');
-        }
-      }, { scope: 'public_profile,email' }); // Specify the required permissions
+  useEffect(() => {
+    /* global google */
+
+    google.accounts.id.initialize({
+      client_id: "403940930490-9p5upakgv7g8brgiignanr4frs6r3rsv.apps.googleusercontent.com",
+      callback: handleCredentialResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("buttonDiv"),
+      { theme: "outline", size: "large" }  // customization attributes
+    );
     
+    google.accounts.id.prompt(); // also display the One Tap dialog
+
+  }, []);
+
+  const handleResponse = (response) => {
+    if (response.credential) {
+        // User is authenticated, you can access user details from response.credential
+        const idToken = response.credential;
+        console.log('Google user ID token:', idToken);
+        // Handle user data and authentication here
+    } else {
+        // User canceled or failed to authenticate
+        console.error('Google login canceled or failed');
+    }
+  }
+
+  const handleGitHubLogin = () => {
+      // Redirect the user to GitHub OAuth page
+      window.location.href = `https://github.com/login/oauth/authorize?client_id=YOUR_GITHUB_CLIENT_ID&scope=user`;
   };
 
-
-    useEffect(() => {
-        /* global google */
-        google.accounts.id.initialize({
-          client_id: "403940930490-9p5upakgv7g8brgiignanr4frs6r3rsv.apps.googleusercontent.com",
-          callback: handleResponse
-        })
-    
-        google.accounts.id.renderButton(
-          document.getElementById("signInDiv"),
-          {theme: "outline", size: "large"}
-        );
-    
-        // google.accounts.id.prompt();
-    
-      }, []);
-
-    //   const handleResponse = (response) => {
-    //     if (response.credential) {
-    //         // User is authenticated, you can access user details from response.credential
-    //         const idToken = response.credential;
-    //         console.log('Google user ID token:', idToken);
-    //         // Handle user data and authentication here
-    //     } else {
-    //         // User canceled or failed to authenticate
-    //         console.error('Google login canceled or failed');
-    //     }
-    // }
-
-    const handleGoogleLogin = async () => {
-        
-
-    
-        //   const handleResponse = (response) => {
-        //     if (response.credential) {
-        //         // User is authenticated, you can access user details from response.credential
-        //         const idToken = response.credential;
-        //         console.log('Google user ID token:', idToken);
-        //         // Handle user data and authentication here
-        //     } else {
-        //         // User canceled or failed to authenticate
-        //         console.error('Google login canceled or failed');
-        //     }
-        
-        // }
-      };
-
-      const handleResponse = (response) => {
-        if (response.credential) {
-            // User is authenticated, you can access user details from response.credential
-            const idToken = response.credential;
-            console.log('Google user ID token:', idToken);
-            // Handle user data and authentication here
-        } else {
-            // User canceled or failed to authenticate
-            console.error('Google login canceled or failed');
-        }
-
-    // const handleGoogleLogin = () => {
-    //     // Define the configuration for Google Identity Services
-    //     const gsiConfig = {
-    //       client_id: '403940930490-9p5upakgv7g8brgiignanr4frs6r3rsv.apps.googleusercontent.com',
-    //     };
-    
-    //     // Initialize Google Identity Services
-    //     const gsi = google.accounts.id.initialize(gsiConfig);
-    
-    //     // Prompt the user to select a Google account for authentication
-    //     gsi.prompt()
-    //       .then((response) => {
-    //         if (response.credential) {
-    //           // User is authenticated, you can access user details from response.credential
-    //           const idToken = response.credential;
-    //           console.log('Google user ID token:', idToken);
-    //           // Handle user data and authentication here
-    //         } else {
-    //           // User canceled or failed to authenticate
-    //           console.error('Google login canceled or failed');
-    //         }
-    //       })
-    //       .catch((error) => {
-    //         console.error('Error during Google login:', error);
-    //       });
-      };
-
-    const handleGitHubLogin = () => {
-        // Redirect the user to GitHub OAuth page
-        window.location.href = `https://github.com/login/oauth/authorize?client_id=YOUR_GITHUB_CLIENT_ID&scope=user`;
-    };
-      
-
-    const handleLinkedInLogin = () => {
-        // Authenticate using the LinkedIn SDK
-        IN.User.authorize(function () {
-          // User is authenticated, you can fetch user details using the LinkedIn API
-          IN.API.Profile('me').result(function (user) {
-            console.log('LinkedIn user:', user.values[0]);
-            // Handle user data and authentication here
-          });
+  const handleLinkedInLogin = () => {
+      // Authenticate using the LinkedIn SDK
+      IN.User.authorize(function () {
+        // User is authenticated, you can fetch user details using the LinkedIn API
+        IN.API.Profile('me').result(function (user) {
+          console.log('LinkedIn user:', user.values[0]);
+          // Handle user data and authentication here
         });
-    };
+      });
+  };
     
+  function handleCredentialResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+  }
     
-    return (
-        <div>
-            
-            <h1>Choose your login method below.</h1>
-            
-            <div id='wrapper'>
+  return (
+    <div>
+      <h1>Choose your login method below.</h1>
+        
+      <div id='wrapper'>
 
-            <div onClick={handleFacebookLogin} > 
-                <img src={""} alt='' /> 
-                Facebook 
-            </div>
-            <br></br>
-            <div onClick={handleGoogleLogin} >
-            <div id="signInDiv" ></div>
-                <img img src={""} alt='' />      
-                Google 
-            </div>
-            <br></br>
-            <div onClick={handleGitHubLogin} > 
-                <img img src={""} alt='' /> 
-                Github 
-            </div>
-            <br></br>
-            <div onClick={handleLinkedInLogin} > 
-                <img img src={""} alt='' /> 
-                LinkedIn 
-            </div>
-            <br></br>
-            </div>
+        <br></br>
+      
+        <div id="buttonDiv"></div>
+        
+        <br></br>
+        
+        <div onClick={handleGitHubLogin} > 
+            Github 
         </div>
-    );
+        
+        <br></br>
+        
+        <div onClick={handleLinkedInLogin} > 
+            LinkedIn 
+        </div>
+        
+        <br></br>
+        
+      </div>
+    </div>
+  );
 }
   
 
